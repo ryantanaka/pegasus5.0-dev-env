51cbacfccf0324b53ae5639c099cc4272ec0ad92
Ryan Tanaka <ryanyt@hawaii.edu>
2020-06-01 13:46:55 -0700

    moved shared-data permissions update

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@
 
 set -e 
 
+sudo chown :808 ./shared-data \
+    && chmod 775 ./shared-data \
+    && chmod g+s ./shared-data
+
 docker image build \
     --build-arg BUILD_DATE=$(date +%Y-%m-%d_%H:%M:%S) \
     -t ryantanaka/pegasus5-condor .
 
-sudo chown :808 ./shared-data \
-    && chmod 775 ./shared-data \
-    && chmod g+s ./shared-data
 
 
