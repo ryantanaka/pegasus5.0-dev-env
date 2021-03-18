aae990fccd22bd0e5e6786e86473b7955132054e
Ryan Tanaka <tanaka@isi.edu>
2021-03-18 16:35:32 -0700

    removed unused arg from build script

--- build.sh
+++ build.sh
@@ -8,9 +8,7 @@ sudo chown :808 ./shared-data \
     && chmod 775 ./shared-data \
     && chmod g+s ./shared-data
 
-docker image build \
-    --build-arg BUILD_DATE=$(date +%Y-%m-%d_%H:%M:%S) \
-    -t ryantanaka/pegasus5-condor .
+docker image build -t ryantanaka/pegasus5-condor .
 
 
 
