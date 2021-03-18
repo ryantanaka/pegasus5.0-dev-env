2564040fa29e5de75c86bf8e8d7f8f9cae045cb6
Ryan Tanaka <ryanyt@hawaii.edu>
2021-03-18 14:07:06 -0700

    updated run script to use docker nvidia runtime

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+set -v
+
+#RUNTIME="runc"
+RUNTIME="nvidia"
 
 docker container run \
     --shm-size=1024m \
     -p 8888:8888 \
     -p 5000:5000 \
+    --runtime $RUNTIME \
     --privileged \
     --mount type=bind,source="$(pwd)"/shared-data,target=/home/scitech/shared-data \
     ryantanaka/pegasus5-condor
